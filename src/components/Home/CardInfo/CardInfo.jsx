ba0cbf794bd9a2e776ed3d4456798ec007a950bb
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-11-14 20:57:30 +0600

    Successfully Completed

---
+++ src/components/Home/CardInfo/CardInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CardInfo = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default CardInfo;
---
+++ src/components/Home/DentalCare/DentalCare.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DentalCare = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default DentalCare;
